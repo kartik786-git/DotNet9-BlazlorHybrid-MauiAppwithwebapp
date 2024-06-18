6898d2dc6b0f970e9a9d9e92abad07e6ff0316b7
kartik <kartik.kumar53@gmail.com>
2024-06-18 16:09:06 +0530

    Add project files.

---
+++ MauiAppwithwebapp/MauiAppwithwebapp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppwithwebapp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiAppwithwebapp/MauiAppwithwebapp/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using MauiAppwithwebapp.Services;
+using MauiAppwithwebapp.Shared.Services;
+using Microsoft.Extensions.Logging;
+
+namespace MauiAppwithwebapp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            // Add device-specific services used by the MauiAppwithwebapp.Shared project
+            builder.Services.AddSingleton<IFormFactor, FormFactor>();
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
